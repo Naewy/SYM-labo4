c6b6dd6d0f32d95193f4cb3d9ca8b18dbbee779b
Naewy <katomey@gmail.com>
2018-01-19 15:08:48 +0100

    3ème type de notification ok

--- application/mobile/src/main/java/ch/heigvd/iict/sym/sym_labo4/NotificationActivity.java
+++ application/mobile/src/main/java/ch/heigvd/iict/sym/sym_labo4/NotificationActivity.java
@@ -1,11 +1,13 @@
 package ch.heigvd.iict.sym.sym_labo4;
 
+import android.app.Notification;
 import android.app.PendingIntent;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.NotificationCompat;
 import android.support.v4.app.NotificationManagerCompat;
+import android.support.v4.app.RemoteInput;
 import android.support.v4.app.TaskStackBuilder;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -78,6 +80,43 @@ public class NotificationActivity extends AppCompatActivity {
 
                 // Issue the notification with notification manager.
                 notificationManager.notify(02, notificationBuilder.build());
+            }
+        });
+
+        buttonWearables.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent mapIntent = new Intent(Intent.ACTION_VIEW);
+                // The channel ID of the notification.
+                Uri geoUri = Uri.parse("geo:0,0?q=" + Uri.encode("Yverdon-les-Bains"));
+                mapIntent.setData(geoUri);
+                PendingIntent mapPendingIntent =
+                    PendingIntent.getActivity(NotificationActivity.this, 0, mapIntent, 0);
+
+                String id = "my_channel_03";
+
+                // Create a WearableExtender to add functionality for wearables
+                NotificationCompat.WearableExtender wearableExtender =
+                    new NotificationCompat.WearableExtender()
+                        .addAction(new NotificationCompat.Action(R.drawable.common_google_signin_btn_icon_light, getString(R.string.app_name), mapPendingIntent))
+                        .setHintHideIcon(true);
+
+                // Create a NotificationCompat.Builder to build a standard notification
+                // then extend it with the WearableExtender
+                Notification notif = new NotificationCompat.Builder(NotificationActivity.this, id)
+                        .setContentTitle("Toto3")
+                        .setContentText("Yverdon-les-bains de sang")
+                        .setSmallIcon(R.drawable.common_google_signin_btn_icon_light)
+                        .extend(wearableExtender)
+                        .build();
+
+                // Get an instance of the NotificationManager service
+                NotificationManagerCompat notificationManager = NotificationManagerCompat.from(NotificationActivity.this);
+
+                // Issue the notification with notification manager.
+                notificationManager.notify(02, notif);
+
             }
         });
     }
